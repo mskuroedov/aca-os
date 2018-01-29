7fac4c3cf77e6a3811ac35ab151896794aff1a78
kuroedov_maxim <Ni7pfpNh>
2018-01-29 11:58:12 +0300

    school header

--- src/components/schools/list/SchoolListPage.js
+++ src/components/schools/list/SchoolListPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Subheader from "../../../Subheader";
 import Header from "../../../Header";
+import SchoolHeader from "./SchoolHeader";
 
 export default class SchoolListPage extends React.Component {
     render() {
@@ -8,7 +9,7 @@ export default class SchoolListPage extends React.Component {
             <div>
                 <Header />
                 <Subheader />
-                <p>SchoolListPage</p>
+                <SchoolHeader />
 
             </div>
         )
